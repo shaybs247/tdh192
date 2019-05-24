dad4fd6d5195be4c0862f5c3ce0cf5c1d965cca4
Shay Ben Simon <shay.bs@spot.im>
2019-05-24 19:25:15 +0300

    urls

---
+++ constants.js
@@ -0,0 +1,3 @@
+export const CINEMA_OF_ISRAEL = `https://www.cinemaofisrael.co.il/%D7%90%D7%99%D7%A0%D7%93%D7%A7%D7%A1/`;
+export const ALPHABET = `.alphabet `;
+export const MOVIES_LIST = `.linklist.linkfx`;
---
+++ index.js
@@ -0,0 +1,50 @@
+/**
+ * Scraping data from the Cinema of Israel site.
+ *
+ * 1. Script will traverse all pages on the site and scrape the properties of each movie
+ * 2. Script will make a CVS out of the extracted information
+ *
+ * Authors: Shay Ben-Simon and Lior Mizrahi
+ */
+
+import puppeteer from "puppeteer";
+import * as C from "./constants";
+
+// Get Aleph-Bet index elements
+const getAlephBetIndexes = async page => {
+  const abcListContainer = await page.$(C.ALPHABET);
+  const listElements = await abcListContainer.$$("li");
+
+  return listElements;
+};
+
+const getMovieUrls = async (page, letter) => {
+  await letter.click();
+  await page.waitForSelector(C.MOVIES_LIST);
+  await page.waitForSelector(".roll");
+  const moviesUList = await page.$(C.MOVIES_LIST);
+  await page.waitForSelector("[data-title]");
+
+  return await moviesUList.$$eval("a", anchors =>
+    anchors.map(el => el.getAttribute("href"))
+  );
+};
+
+const run = async () => {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  await page.goto(`${C.CINEMA_OF_ISRAEL}#`);
+  const abcElements = await getAlephBetIndexes(page);
+
+  // await getMovieUrls(page, await abcElements[0].$("a"));
+  let movies_urls = await Promise.all(
+    abcElements.map(async li => await getMovieUrls(page, await li.$("a")))
+  );
+  movies_urls = movies_urls.map(suffixes =>
+    suffixes.map(suffix => C.CINEMA_OF_ISRAEL.concat(suffix))
+  );
+  console.log(movies_urls);
+  await browser.close();
+};
+
+run();
