42779a913f003a6c307b0a3c843b0cc39aa6afcd
lmiz100 <46241164+lmiz100@users.noreply.github.com>
2019-08-05 18:31:19 +0300

    updated: draw_graph, find_connection, find_all_connections
    find_all_connections is new query

--- network graph.py
+++ network graph.py
@@ -39,25 +39,30 @@ def get_movie_cast(movieRec):
 #input: graph g X flag edgeData X source and target nodes. When edgeData = True , present which movie connect the entities
 #output: g chart
 def draw_graph(g, edgeData, source, target):
-    pos = nx.spring_layout(g)
+    curr_node = target
+    pos = {}
+    for x in range (0, len(g.nodes)):
+        pos[curr_node] = [x,x]
+        curr_node = next(g.neighbors(curr_node))
 
     internal_nodes = []
     for n in g.nodes:
         if n != source and n != target:
             internal_nodes.append(n)
 
+
     nx.draw_networkx_nodes(g, pos,
                            nodelist= [source, target],
                            node_color='red',
-                           node_size=600,
-                           alpha=0.8)
+                           node_size=400,
+                           alpha=0.8,)
     nx.draw_networkx_nodes(g, pos,
                            nodelist= internal_nodes,
                            node_color='blue',
-                           node_size=500,
+                           node_size=400,
                            alpha=0.8)
 
-    nx.draw_networkx_edges(g, pos, width=1.0, alpha=0.5)
+    nx.draw_networkx_edges(g, pos, width=2.0, alpha=0.6)
 
     #graph labels
     node_labels = {}
@@ -69,10 +74,15 @@ def draw_graph(g, edgeData, source, target):
         edge_labels = {e: g.get_edge_data(*e)["טרסה םש"] for e in g.edges}
         nx.draw_networkx_edge_labels(g, pos, edge_labels)
 
-
     nx.draw_networkx_labels(g, pos, node_labels, font_size=16)
     plt.axis('off')
-    plt.savefig("connection_graph.png")  # save as png
+    # adjust chart boundaries
+    x_values, y_values = zip(*pos.values())
+    x_max = max(x_values)
+    x_min = min(x_values)
+    x_margin = (x_max - x_min) * 0.25
+    plt.xlim(x_min - x_margin, x_max + x_margin)
+    plt.savefig("connection_graph.png", bbox_inches = "tight")  # save as png
     plt.show()
 
     return
@@ -108,13 +118,13 @@ def create_relations_graph():
 
 
 #input: name of json graph file to restore X first entity name X second entity name
+#output: connection information as dictionary, connection chart
 #restore relations graph from json,find and show connection between name1 and name2 (if exists)
 def find_connection(graph_file, name1, name2):
     try:
         with open(graph_file, 'r') as json_file:
             restore_graph = json_graph.adjacency_graph(json.load(json_file))
             connection = nx.Graph()
-
 
             relation_path = nx.shortest_path(restore_graph, source = name1, target = name2)
             #name right to left
@@ -128,15 +138,72 @@ def find_connection(graph_file, name1, name2):
 
             draw_graph(connection, True, relation_rtl[0], relation_rtl[len(relation_rtl) - 1])
 
+            connection_res = {}
+            for x in range(0, len(relation_path) - 1):
+                e = (relation_path[x], relation_path[x + 1])
+                connection_res[e] = restore_graph.get_edge_data(*e)["שם הסרט"]
+
     except FileNotFoundError:
         print("Failed open json file")
     except:
         print("There is no connection between " , name1, " and ", name2)
 
-    return
+    return connection_res
+
+
+
+#similar to find_connection, but this method find all connection with at most maximal_length connecting movies (edges)
+# user stops function runnig when type '0' to stdin
+# return array icludes information of each connection which has found
+def find_all_connections(graph_file, name1, name2, maximal_length):
+
+    try:
+        with open(graph_file, 'r') as json_file:
+            restore_graph = json_graph.adjacency_graph(json.load(json_file))
+            path_iterator = nx.all_simple_paths(restore_graph, source= name1, target= name2, cutoff= maximal_length)
+            connections_res = []
+
+            while True:
+                connection = nx.Graph()
+
+                relation_path = next(path_iterator)
+
+                #name right to left
+                relation_rtl = [bidialg.get_display(y) for y in relation_path]
+                nx.add_path(connection, relation_rtl)
+
+                # add movie which connect
+                for x in range(0, len(relation_path) - 1):
+                    movie_name = bidialg.get_display(restore_graph[relation_path[x]][relation_path[x + 1]]["שם הסרט"])
+                    connection[relation_rtl[x]][relation_rtl[x + 1]][bidialg.get_display("שם הסרט")] = movie_name
+
+                draw_graph(connection, True, relation_rtl[0], relation_rtl[len(relation_rtl) - 1])
+
+                #append connection information
+                temp_res = {}
+                for x in range(0, len(relation_path) - 1):
+                    e = (relation_path[x], relation_path[x + 1])
+                    temp_res[e] = restore_graph.get_edge_data(*e)["שם הסרט"]
+
+                connections_res.append(temp_res)
+
+                txt = input("To finish write 0, to get next path write 1 (or anything else) \n")
+                if txt == "0":
+                    break
+
+    except FileNotFoundError:
+        print("Failed open json file")
+    except StopIteration:
+        print("No more connections")
+    except:
+        print("There is no connection in length ", maximal_length, " between " , name1, " and ", name2)
+
+    return connections_res
 
 
 
 
-#this is the query
+#this is the queries
 find_connection("json_graph.json", "גיל רוזנטל", "פיני טבגר")
+# print(find_all_connections("json_graph.json", "גיל רוזנטל", "פיני טבגר", 8)) #find every connection with maximal length 10
+
